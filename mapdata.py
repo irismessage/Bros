eb60571120170c66dde6fe3b467e1ba74c4f72bb
jmcb <joelsgp@protonmail.com>
2022-05-01 23:06:52 +0100

    More work on mapdata script

--- mapdata.py
+++ mapdata.py
@@ -1,6 +1,3 @@
-import pyperclip
-
-
 def process(mapdata: list[str]) -> str:
     processed = []
     for l in mapdata:
@@ -8,25 +5,40 @@ def process(mapdata: list[str]) -> str:
     return ''.join(processed)
 
 
-def multilineinput() -> list[str]:
-    lines = []
+def deprocess(mapdata: str) -> list[str]:
+    processed = []
+    for i in range(0,512,32):
+        processed.append(mapdata[i:i+32])
+    return processed
+
+
+def seekto(text: str):
+    file = open('cart/bros.p8', encoding='utf-8')
     while True:
-        i = input()
-        if not i:
+        line = file.readline()
+        if line == text + '\n' or not line:
             break
-        lines.append(i)
-    return lines
+    return file
+
+
+def seektoscrn(file, scrn: int):
+    for i in range(scrn):
+        line = file.readline()
+        if line == '}\n':
+            break
+
+
+def writeencoded(mapdata: list[str], scrn: int):
+    file  = seekto('screens = {')
+    seektoscrn(file, scrn)
 
 
 def readmap() -> list[str]:
     maplines = []
-    with open('cart/bros.p8', encoding='utf-8') as file:
-        while True:
-            line = file.readline()
-            if line == '__map__\n':
-                break
-        for i in range(16):
-            maplines.append(file.readline())
+    file = seekto('__map__')
+    for i in range(16):
+        maplines.append(file.readline())
+    file.close()
     return maplines
         
 
