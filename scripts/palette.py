73c5c9240080410f3c52eb3872b6ef14b473ffa8
jmcb <joelsgp@protonmail.com>
2022-08-19 19:50:53 +0100

    Work on colour palettes

---
+++ scripts/palette.py
@@ -0,0 +1,51 @@
+# 0 black shading, clock
+# 1 light orange, text
+# 2 dark orange, bricks
+# 3 red, clothes, mushroom
+# 4 dark blue, background
+
+dark_grey_shade = '252525'
+lorange_text = 'be7917'
+dorange_bricks = '813c01'
+red = '73003a'
+black = '000000'
+dark_blue_bg = '203ecb'
+light_grey = '454545'
+pale_grey = '838383'
+red_but_scary = '6f0a00'
+kooky_violet = '4563ef'
+
+palette = {
+    11: (dark_grey_shade, lorange_text, dorange_bricks, red, dark_blue_bg),
+    12: (dark_grey_shade, lorange_text, '005f88', red, black),
+    13: 11,
+    14: (light_grey, lorange_text, dorange_bricks, red, black),
+    21: 11,
+    22: (dark_grey_shade, lorange_text, dark_blue_bg, red, black),
+    23: (dark_grey_shade, lorange_text, dark_blue_bg, red, '001daa'),
+    24: (dark_grey_shade, lorange_text, '6a6a6a', red, black),
+    31: (dark_grey_shade, lorange_text, dorange_bricks, red, '4f0014'),
+    32: (dark_grey_shade, lorange_text, '007231', red, black),
+    33: 31,
+    34: 32,
+    41: (dark_grey_shade, lorange_text, dorange_bricks, red, light_grey),
+    42: (light_grey, lorange_text, '601b00', red, black),
+    43: (dark_grey_shade, lorange_text, dorange_bricks, red, light_grey),
+    44: (light_grey, lorange_text, dark_grey_shade, red, black),
+    51: 11,
+    52: (dark_grey_shade, lorange_text, dorange_bricks, red, black),
+    53: (dark_grey_shade, lorange_text, dorange_bricks, red, light_grey),
+    54: 52,
+    61: (black, pale_grey, light_grey, red_but_scary, '000085'),
+    62: (black, pale_grey, light_grey, red_but_scary, black),
+    63: (black, pale_grey, dark_grey_shade, red_but_scary, '3200b8'),
+    64: (dark_grey_shade, lorange_text, dorange_bricks, red, light_grey),
+    71: (dark_grey_shade, 'c04feb', '902c02', red, '203ecb'),
+    72: ('a6a6a6', '5d7dff', '6a6a6a', '8212ad', black),
+    73: (dark_grey_shade, lorange_text, '5421da', '951859', kooky_violet),
+    74: (light_grey, lorange_text, black, '941859', black),
+    81: (light_grey, lorange_text, black, kooky_violet, red_but_scary),
+    82: (light_grey, 'e39e3e', black, kooky_violet, black),
+    83: (light_grey, '6a6a6a', black, '5e7cff', red_but_scary),
+    84: (black, lorange_text, black, red, black),
+}
---
+++ scripts/view_worlds_headers.py
@@ -0,0 +1,12 @@
+from _common import mine
+
+
+print(' ' * 22, *(f'{i:0>2x}' for i in range(0,15)), sep='')
+for world in range(1, 8+1):
+    for level in range(1, 4+1):
+        print(f"    {world}{level}: ('', '', '', '', ''),")
+        # path = f'datamined/WORLD{world}{level}.DAT'
+        # data = mine(path, echo=False)
+        # header = data[:15]
+        # header_formatted = header.hex()
+        # print(path, header_formatted)
