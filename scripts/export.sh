c541059c9b1055174492c962391cd3bfd41e6408
jmcb <joelsgp@protonmail.com>
2022-09-12 15:11:39 +0100

    Create export script

---
+++ scripts/export.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+workdir="$(pwd)"
+cart="$workdir"/cart/bros.p8
+
+cd "$workdir"/cart/
+mkdir -p export/
+cd export/
+pico8 "$cart" -export bros.png
+pico8 "$cart" -export bros.p8.png
+pico8 "$cart" -export "-f bros.html"
+pico8 "$cart" -export "-i 2 -c 12 bros.bin"
+
+cp bros.png "$workdir"/assets/spritesheet.png
