9bcd47933cca735387902434e4794ea04178ed29
jmcb <joelsgp@protonmail.com>
2023-01-09 19:35:10 +0000

    Use minified version in export.sh

--- scripts/export.sh
+++ scripts/export.sh
@@ -9,9 +9,13 @@ cart_fmt="$workdir"/cart/bros_fmt.p8
 cd "$workdir"/cart/
 mkdir -p export/
 cd export/
+# spritesheet
 pico8 "$cart" -export bros.png
-pico8 "$cart" -export bros.p8.png
-pico8 "$cart" -export "-f bros.html"
-pico8 "$cart" -export "-i 2 -c 12 bros.bin"
+# cart image
+pico8 "$cart_fmt" -export bros.p8.png
+# cart web player
+pico8 "$cart_fmt" -export "-f bros.html"
+# cart binaries
+pico8 "$cart_fmt" -export "-i 2 -c 12 bros.bin"
 
 cp bros.png "$workdir"/assets/spritesheet.png
